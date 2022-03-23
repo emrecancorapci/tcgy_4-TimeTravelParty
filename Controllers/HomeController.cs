2cde932814374d3a1caabc4d22b08690ed67c460
emrecancorapci <emrecancorapci@gmail.com>
2022-03-23 19:05:51 +0300

    Commit init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using tcgy_4_TimeTravelParty.Models;
+
+namespace tcgy_4_TimeTravelParty.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(Participant participant)
+        {
+            var isParticipating = participant.Participation;
+            var isCovidShot = participant.CovidShot;
+
+            if (isParticipating && isCovidShot)
+                return View("Thanks");
+            if (isParticipating && !isCovidShot)
+                return View();
+            if (!isParticipating)
+                return View("Why");
+            throw new ArgumentOutOfRangeException(nameof(isParticipating));
+        }
+    }
+}
---
+++ Models/Participant.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace tcgy_4_TimeTravelParty.Models
+{
+    public class Participant
+    {
+        [Display(Name = "Name:")]
+        [Required(ErrorMessage ="Name cannot be empty!")]
+        [MinLength(2,ErrorMessage ="Name must be at least 2 characters.")]
+        public string Name { get; set; }
+
+        [Display(Name = "Last Name:")]
+        [Required(ErrorMessage ="Last Name cannot be empty!")]
+        [MinLength(2,ErrorMessage ="Last name must be at least 2 characters.")]
+        public string LastName { get; set; }
+
+        [Display(Name = "E-Mail:")]
+        [Required(ErrorMessage = "E-Mail cannot be empty!")]
+        [EmailAddress(ErrorMessage ="E-Mail is invalid.")]
+        public string Email { get; set; }
+
+        [Display(Name = "Living Date:")]
+        [Required(ErrorMessage = "Your current date cannot be empty.")]
+        public DateTime DateYouLive { get; set; }
+
+        [Display(Name = "Participation:")]
+        public bool Participation { get; set; }
+
+        [Display(Name = "Did you get your covid shot?:")]
+        public bool CovidShot { get; set; }
+    }
+}
